09b04cc72c38d4c0b99f2a01433b23e2e50b14c9
cartrleitch <122408763+cartrleitch@users.noreply.github.com>
2023-04-24 17:48:26 -0500

    Add files via upload

--- main_reimbursements.py
+++ main_reimbursements.py
@@ -4,60 +4,75 @@ import justpy as jp
 
 
 def main():
-    #conn = sqlite3.connect('db_reimbursements.db') each database interaction
-    #cur = conn.cursor()
-    #conn.commit()
-    #conn.close()
-
-    wp = jp.WebPage(data={'text': 'Initial'})
-    button_classes = "w-32 h-10 bg-blue-500 m-2 hover:bg-blue-700 text-white font-bold py-2" \
-                     " px-4 rounded text-center align-baseline"
+    wp = jp.WebPage(delete_flag=False)
+    button_classes = "w-32 h-10 bg-green-500 m-2 hover:bg-green-700 text-white font-bold py-2" \
+                     " px-4 rounded text-center"
     div_classes = "flex m-4 flex-wrap"
-    input_classes = "m-2 bg-gray-200 border-2 border-gray-200 rounded w-64 py-2 px-4 text-gray-700 " \
-                    "focus:outline-none focus:bg-white focus:border-purple-500"
     p_classes = "m-2 p-2 h-10 text-xl"
-    button_div = jp.Div(text='Button label: ', a=wp, classes=div_classes)
 
-    # basic button
-    def click_1(self, msg):
-        self.num_clicked += 1
-        if self.num_clicked % 2 == 0:
-            self.text = 'I was clicked'
-        else:
-            self.text = 'Change'
+    banner = jp.Div(text='Five Oaks Church', a=wp, classes='block uppercase w-full h-1000 bg-blue place-items-top '
+                                                           'font-bold text-xs text-white align-left')
 
-    b = jp.Button(text='Button title', a=button_div, classes=button_classes, click=click_1)
-    b.num_clicked = 0
-    # basic button
+    # basic form (input and action)
+    def input_form():
+        form_div = jp.Div(a=wp, classes='grid h-screen place-items-center')
+        form1 = jp.Form(a=form_div, classes='flex flex-col border m-5 p-4 w-100')
+        input_div = jp.Div(a=form1, classes='py-1 pl-8')
+        button_div = jp.Div(a=form1, classes='py-3')
 
-    # insert entries
-    def inp1_ret(self, msg):
-        self.val = self.value
-        print(self.val)
+        # form entry template
+        minID_label = jp.Label(text='MinistryID',
+                               classes='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-2',
+                               a=input_div)
+        in1 = jp.Input(placeholder='MinistryID', a=input_div, classes='form-input', type='number')
+        minID_label.for_component = in1
+        # form entry template
 
-    async def in1():
-        inp_1 = jp.Input(type='number', a=wp, classes=input_classes, placeholder='Type here', model=[wp, 'text'])
-        inp_1.div = jp.Div(model=['text'], text='MinistryID', classes=p_classes, a=wp)
-        inp_1.on('input', inp1_ret)
-        return wp
+        ministry_label = jp.Label(text='Ministry',
+                                  classes='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-2',
+                                  a=input_div)
+        in2 = jp.Input(placeholder='Ministry', a=input_div, classes='form-input', type='text')
+        ministry_label.for_component = in2
 
-    in1()
-    # insert entries
+        def contents_show(self, msg):
+            conn = sqlite3.connect('db_reimbursements.db')
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM Ministries')
+            results = cur.fetchall()
+            for row in results:
+                print(row)
+            conn.commit()
+            conn.close()
 
-    # insert button
-    def click_insert(self, msg):
-        print(self.val)
+        submit_button = jp.Input(value='Submit Form', type='submit', a=button_div, classes=button_classes)
+        show_button = jp.Button(text='Show Values', type='button', a=button_div, classes=button_classes,
+                                click=contents_show)
 
-    conn = sqlite3.connect('db_reimbursements.db')
-    cur = conn.cursor()
-    b = jp.Button(model=[wp, 'text'], text='Save', a=button_div, classes=button_classes, click=click_insert)
-    b.val = inp1_ret
-    conn.commit()
-    conn.close()
-    # insert button
+        def submit_form(self, msg):
+            min_id = 0
+            desc = ''
 
+            data = msg.form_data
+            for output in data:
+                if output.placeholder == 'MinistryID':
+                    min_id = output.value
 
-# creates webpage
+                if output.placeholder == 'Ministry':
+                    desc = output.value
+
+            conn = sqlite3.connect('db_reimbursements.db')
+            cur = conn.cursor()
+            cur.execute('INSERT INTO Ministries VALUES (?, ?)', (min_id, desc))
+            conn.commit()
+            conn.close()
+
+        form1.on('submit', submit_form)
+
+    input_form()
+
+    # basic form (input and action)
+
+    # creates webpage
     def run():
         return wp
 
